87fb3d81a127a8b59c0e623f14822d6ad0a20ff5
Taiwo Sodiq <klassikklassik3@gmail.com>
2023-03-07 10:14:56 +0100

    added styles to the app

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,53 @@
 
 <script>
 
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return {
+      showAddTask: false
+    }
+  },
+}
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap');
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+body {
+  color: #333;
+  padding: 0 16px;
+}
+
+.container {
+  max-width: 700px;
+  margin: 32px auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 24px;
+  border-radius: 12px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 16px;
+
+}
 </style>
